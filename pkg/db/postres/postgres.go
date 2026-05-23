6e87824f47e0d87083b8b3e40bb0b13b977befb5
Mover-R <denis.tugow2014@gmail.com>
2026-05-23 16:08:03 +0300

    posgres connection

--- pkg/db/postres/postgres.go
+++ pkg/db/postres/postgres.go
@@ -1,1 +1,49 @@
 package postres
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgres(ctx context.Context, cfg PostgresConfig) (*pgxpool.Pool, error) {
+	dsn := cfg.GetDSN()
+
+	poolCfg, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("parse pgxpool config: %w", err)
+	}
+
+	poolCfg.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	if max := cfg.GetMaxConns(); max > 0 {
+		poolCfg.MaxConns = int32(max)
+	}
+	if cfg.MinConns > 0 {
+		poolCfg.MinConns = int32(cfg.MinConns)
+	}
+	if cfg.ConnMaxIdleTime > 0 {
+		poolCfg.MaxConnIdleTime = cfg.ConnMaxIdleTime
+	}
+	if cfg.ConnMaxLifetime > 0 {
+		poolCfg.MaxConnLifetime = cfg.ConnMaxLifetime
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolCfg)
+	if err != nil {
+		return nil, fmt.Errorf("create pgx pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, fmt.Errorf("ping postgres: %w", err)
+	}
+
+	return pool, nil
+}
